a400f4d77da9792919080099b8a1f43a2cc3f476
freelancernishad <freelancernishad123@gmail.com>
2025-01-28 12:29:53 +0600

    update

--- app/Http/Controllers/Api/Admin/Vehicle/VehicleController.php
+++ app/Http/Controllers/Api/Admin/Vehicle/VehicleController.php
@@ -61,7 +61,7 @@ class VehicleController extends Controller
             'fuel_type' => 'nullable|string',
             'length' => 'nullable|string',
             'transmission' => 'nullable|string',
-            'extra_features' => 'nullable',
+            'extra_features' => 'nullable|array',
             'images' => 'nullable|array',
             'images.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
